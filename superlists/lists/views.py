6cb338c1809badc038dbd01c5df9ebdab04cd647
steven-ban <452827500@qq.com>
2017-05-04 18:43:17 +0800

    修改lists.views使首页包含数据库内容。问题：数据库未生成表lists_item

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from models import Item
+
 # Create your views here.
 
-home_page = None
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': item.text,
     })
     # return HttpResponse('<html><title>To-Do lists</title></html>')
 
